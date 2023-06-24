19fb221da555b5a9c4ed7858c82c7faeef51298e
Ayman Al-Qadhi <alqd@protonmail.com>
2023-06-24 21:59:56 +0300

    feat: implement `Encoder` & `Decoder` traits for all integral types

---
+++ recode/src/codec/number.rs
@@ -0,0 +1,39 @@
+use std::convert::Infallible;
+
+macro_rules! impl_int {
+    ($t:ty) => {
+        paste::paste! {
+            impl crate::Encoder for $t {
+                type Input = Self;
+
+                fn encode<B: bytes::BufMut>(
+                    &self,
+                    input: Self::Input,
+                    buf: &mut B,
+                ) {
+                    buf.[<put_ $t>](input)
+                }
+            }
+
+            impl crate::Decoder for $t {
+                type Output = Self;
+                type Error = Infallible;
+
+                fn decode<B: bytes::Buf>(
+                    buf: &mut B,
+                ) -> Result<Self::Output, Self::Error> {
+                    Ok(buf.[<get_ $t>]())
+                }
+            }
+        }
+    };
+}
+
+impl_int!(i8);
+impl_int!(u8);
+impl_int!(i16);
+impl_int!(u16);
+impl_int!(i32);
+impl_int!(u32);
+impl_int!(i64);
+impl_int!(u64);
--- recode/src/lib.rs
+++ recode/src/lib.rs
@@ -1,3 +1,4 @@
+mod codec;
 mod decode;
 mod encode;
 
