5d10a5f29aaa0be20c9fb0d941a60996428ea7b0
Ayman Al-Qadhi <alqd@protonmail.com>
2023-06-24 21:44:42 +0300

    feat: add a trait to represent decoders

---
+++ recode/src/decode.rs
@@ -0,0 +1,20 @@
+use bytes::Buf;
+
+/// A trait for types that can decode values of type [`Decoder::Output`] from
+/// a bytes buffer.
+pub trait Decoder {
+    /// The type of the value that will be decoded.
+    type Output;
+
+    /// The type of error that can occur if decoding fails.
+    type Error;
+
+    /// Decodes a value from the given buffer.
+    ///
+    /// # Arguments
+    /// * `buf` - The buffer to decode the value from.
+    ///
+    /// # Returns
+    /// The decoded value.
+    fn decode<B: Buf>(buf: &B) -> Result<Self::Output, Self::Error>;
+}
