34edf0b8e717ff8a368e3002a595acd1e3237f97
Ayman Al-Qadhi <alqd@protonmail.com>
2023-06-25 18:37:23 +0300

    feat: add a utility trait to enable using any* numerical value as a length prefix value

---
+++ recode/src/codec/length.rs
@@ -0,0 +1,22 @@
+use crate::decode::Decoder;
+use crate::error::{Error, Result};
+
+pub trait LengthTraits: Decoder<Output = Self> + Sized
+where
+    usize: TryFrom<Self>,
+    Error: From<<Self as Decoder>::Error>,
+    Error: From<<usize as TryFrom<Self>>::Error>,
+{
+    const BYTE_COUNT: usize = std::mem::size_of::<Self>();
+
+    fn decode_usize<B: bytes::Buf>(buf: &mut B) -> Result<usize> {
+        let value = Self::decode(buf)?;
+
+        Ok(value.try_into()?)
+    }
+}
+
+impl LengthTraits for u16 {}
+impl LengthTraits for u32 {}
+impl LengthTraits for u64 {}
+impl LengthTraits for u128 {}
