696a44b0eb10a81a79fc1bf32568e49d7ab03254
Ayman Al-Qadhi <alqd@protonmail.com>
2023-06-26 00:49:27 +0300

    ref: simplify proc macro declaration

--- recode-derive/src/lib.rs
+++ recode-derive/src/lib.rs
@@ -3,37 +3,16 @@ mod decoder;
 use darling::{FromDeriveInput, ToTokens};
 use proc_macro::TokenStream;
 
-macro_rules! parse_derive_input {
-    ($input:ident => $parser:ty) => {{
-        let input = syn::parse_macro_input!($input as syn::DeriveInput);
-
-        let ts: TokenStream = match <$parser>::from_derive_input(&input) {
-            | Ok(tokens) => tokens,
-            | Err(err) => return err.write_errors().into(),
-        }
-        .into_token_stream()
-        .into();
-
-        ts
-    }};
-}
-
-macro_rules! define_proc_macro {
-    ($trait:ident with $impl:ty [ $($attr:ident),* ]) => {
-        paste::paste! {
-            #[proc_macro_error::proc_macro_error]
-            #[proc_macro_derive($trait, attributes($($attr),*))]
-            pub fn [<derive_ $trait:snake>](input: TokenStream) -> TokenStream {
-                parse_derive_input!(input => $impl).into()
-            }
-        }
-    };
-
-    ($trait:ident [ $($attr:ident),* ]) => {
-        paste::paste! {
-            define_proc_macro!($trait with [<$trait:snake>]::$trait [ $($attr),* ]);
+macro_rules! emit_impl_or_error {
+    ($m:ty [$i:ident]) => {
+        match <$m>::from_derive_input(&syn::parse_macro_input!($i)) {
+            | Ok(val) => val.into_token_stream(),
+            | Err(err) => err.write_errors(),
         }
     };
 }
 
-define_proc_macro!(Decoder[decoder, skip, skip_if, map]);
+#[proc_macro_derive(Decoder, attributes(decoder, skip, skip_if, map))]
+pub fn derive_decoder(input: TokenStream) -> TokenStream {
+    emit_impl_or_error!(decoder::Decoder[input]).into()
+}
