563eded4806728a200739b8b953bd7e415707914
Ayman Al-Qadhi <alqd@protonmail.com>
2023-06-25 20:19:51 +0300

    ref: add an associated type to represent possible errors returned by `Encode`

--- recode/src/encode.rs
+++ recode/src/encode.rs
@@ -2,8 +2,8 @@ use bytes::BufMut;
 
 /// A trait to be implemented by types that encode [`Encoder::Input`] values.
 pub trait Encoder {
-    /// The type of the data this encoder is capable of encoding.
-    type Input;
+    /// The type of error that can occur if encoding fails.
+    type Error;
 
     /// Encodes the given input into the output buffer.
     ///
@@ -11,7 +11,9 @@ pub trait Encoder {
     /// [`Encoder::encode`] never do.
     ///
     /// # Arguments
-    /// * `input` - The input to encode.
     /// * `buf` - The output buffer to write the encoded input to.
-    fn encode<B: BufMut>(&self, input: Self::Input, buf: &mut B);
+    fn encode<B: BufMut>(
+        &self,
+        buf: &mut B,
+    ) -> Result<(), Self::Error>;
 }
