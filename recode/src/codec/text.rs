9c7c824aa7cc3d85f9bc6e676db05792682e40f1
Ayman Al-Qadhi <alqd@protonmail.com>
2023-06-25 21:31:48 +0300

    ref: reduce repetition by using a macro to generate encoding implementation

--- recode/src/codec/text.rs
+++ recode/src/codec/text.rs
@@ -1,93 +1,69 @@
 use super::Buffer;
 use crate::{Decoder, Encoder, Error};
 
-/// A marker type to indicate that [`Text`] object is encoded/decoded as ASCII.
-#[derive(Debug)]
-pub struct Ascii<L = ()>(Buffer<L>);
+macro_rules! define_encoding {
+    ($name:ident; doc: $d:literal; validate: |$i:ident| $v:stmt) => {
+        #[derive(Debug)]
+        #[doc = $d]
+        pub struct $name<L = ()>(Buffer<L>);
 
-/// A marker type to indicate that [`Text`] object is encoded/decoded as UTf-8.
-#[derive(Debug)]
-pub struct Utf8<L = ()>(Buffer<L>);
+        impl<L> Decoder for $name<L>
+        where
+            Buffer<L>: Decoder<Output = Buffer<L>>,
+            Error: From<<Buffer<L> as Decoder>::Error>,
+        {
+            type Output = Self;
+            type Error = Error;
 
-impl<L> Decoder for Ascii<L>
-where
-    Buffer<L>: Decoder<Output = Buffer<L>>,
-    Error: From<<Buffer<L> as Decoder>::Error>,
-{
-    type Output = Self;
-    type Error = Error;
+            fn decode<B: bytes::Buf>(buf: &mut B) -> Result<Self::Output, Self::Error> {
+                let $i = Buffer::<L>::decode(buf)?;
 
-    fn decode<B: bytes::Buf>(buf: &mut B) -> Result<Self::Output, Self::Error> {
-        let inner = Buffer::<L>::decode(buf)?;
+                $v
 
+                Ok(Self($i))
+            }
+        }
+
+        impl<L> Encoder for $name<L>
+        where
+            Buffer<L>: Encoder,
+            Error: From<<Buffer<L> as Encoder>::Error>,
+        {
+            type Error = Error;
+
+            #[inline(always)]
+            fn encode<B: bytes::BufMut>(&self, buf: &mut B) -> Result<(), Self::Error> {
+                Ok(self.0.encode(buf)?)
+            }
+        }
+
+        impl<L> std::ops::Deref for $name<L> {
+            type Target = str;
+
+            fn deref(&self) -> &Self::Target {
+                let buf = self.0.deref();
+
+                unsafe { std::str::from_utf8_unchecked(buf.as_ref()) }
+            }
+        }
+    };
+}
+
+define_encoding! {
+    Ascii;
+    doc: "A type that represents a text encoded as ASCII";
+    validate: |inner| {
         if !inner.is_ascii() {
             return Err(TextError::Ascii("invalid ascii data"))?;
         }
-
-        Ok(Self(inner))
     }
 }
 
-impl<L> Encoder for Ascii<L>
-where
-    Buffer<L>: Encoder,
-    Error: From<<Buffer<L> as Encoder>::Error>,
-{
-    type Error = Error;
-
-    #[inline(always)]
-    fn encode<B: bytes::BufMut>(&self, buf: &mut B) -> Result<(), Self::Error> {
-        Ok(self.0.encode(buf)?)
-    }
-}
-
-impl<L> Decoder for Utf8<L>
-where
-    Buffer<L>: Decoder<Output = Buffer<L>>,
-    Error: From<<Buffer<L> as Decoder>::Error>,
-{
-    type Output = Self;
-    type Error = Error;
-
-    fn decode<B: bytes::Buf>(buf: &mut B) -> Result<Self::Output, Self::Error> {
-        let inner = Buffer::<L>::decode(buf)?;
-
+define_encoding! {
+    Utf8;
+    doc: "A type that represents a text encoded as UTF-8";
+    validate: |inner| {
         _ = std::str::from_utf8(inner.as_ref()).map_err(TextError::Utf8)?;
-
-        Ok(Self(inner))
-    }
-}
-
-impl<L> Encoder for Utf8<L>
-where
-    Buffer<L>: Encoder,
-    Error: From<<Buffer<L> as Encoder>::Error>,
-{
-    type Error = Error;
-
-    #[inline(always)]
-    fn encode<B: bytes::BufMut>(&self, buf: &mut B) -> Result<(), Self::Error> {
-        Ok(self.0.encode(buf)?)
-    }
-}
-
-impl<L> std::ops::Deref for Ascii<L> {
-    type Target = str;
-
-    fn deref(&self) -> &Self::Target {
-        let buf = self.0.deref();
-
-        unsafe { std::str::from_utf8_unchecked(buf.as_ref()) }
-    }
-}
-
-impl<L> std::ops::Deref for Utf8<L> {
-    type Target = str;
-
-    fn deref(&self) -> &Self::Target {
-        let buf = self.0.deref();
-
-        unsafe { std::str::from_utf8_unchecked(buf.as_ref()) }
     }
 }
 
