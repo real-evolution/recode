3a56947c1c14b31802d3f36664288fcbbcfecbb4
Ayman Al-Qadhi <alqd@protonmail.com>
2023-06-26 18:36:13 +0300

    feat: add optional support for some of `uX` crate types

--- recode/src/codec/mod.rs
+++ recode/src/codec/mod.rs
@@ -2,5 +2,11 @@ mod number;
 mod text;
 mod buffer;
 
+#[cfg(feature = "ux")]
+mod ux;
+
 pub use text::*;
 pub use buffer::*;
+
+#[cfg(feature = "ux")]
+pub use self::ux::*;
---
+++ recode/src/codec/ux.rs
@@ -0,0 +1,56 @@
+pub use ux::{i24, i40, i48, i56, u24, u40, u48, u56};
+
+macro_rules! impl_ux {
+    ($t:ty; size: $s:literal; rep: $r:ty ) => {
+        impl crate::Encoder for $t {
+            type Error = std::convert::Infallible;
+
+            fn encode<B: bytes::BufMut>(
+                &self,
+                buf: &mut B,
+            ) -> Result<(), Self::Error> {
+                let bytes = &<$r>::from(*self).to_be_bytes()[..$s];
+
+                buf.put_slice(bytes);
+
+                Ok(())
+            }
+        }
+
+        impl crate::Decoder for $t {
+            type Output = Self;
+            type Error = crate::Error;
+
+            fn decode<B: bytes::Buf>(
+                buf: &mut B,
+            ) -> Result<Self::Output, Self::Error> {
+                const REPR_LEN: usize = std::mem::size_of::<$r>();
+
+                if buf.remaining() < $s {
+                    return Err(crate::Error::BytesNeeded {
+                        needed: $s - buf.remaining(),
+                        full_len: $s,
+                        available: buf.remaining(),
+                    });
+                }
+
+                let mut be_repr = [0u8; REPR_LEN];
+                buf.copy_to_slice(&mut be_repr[(REPR_LEN - $s)..REPR_LEN]);
+
+                Ok(<$t>::new(<$r>::from_be_bytes(be_repr)))
+            }
+        }
+    };
+}
+
+impl_ux!(i24; size: 3; rep: i32);
+impl_ux!(u24; size: 3; rep: u32);
+
+impl_ux!(i40; size: 5; rep: i64);
+impl_ux!(u40; size: 5; rep: u64);
+
+impl_ux!(i48; size: 6; rep: i64);
+impl_ux!(u48; size: 6; rep: u64);
+
+impl_ux!(i56; size: 7; rep: i64);
+impl_ux!(u56; size: 7; rep: u64);
