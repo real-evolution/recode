662c5a6a61902ceb135866b8ad673a3d45519585
Ayman Al-Qadhi <alqd@protonmail.com>
2023-07-01 22:56:00 +0300

    ref: make `util` module a directory modul

---
+++ recode-derive/src/util/mod.rs
@@ -0,0 +1,2 @@
+mod r#type;
+pub(crate) use r#type::{box_type, default_buffer_name, str_to_type};
--- recode-derive/src/util/type.rs
+++ recode-derive/src/util/type.rs
@@ -1,15 +1,15 @@
 use syn::parse::{Parse, Parser};
 
-pub(super) fn str_to_type(ty: &str) -> syn::Type {
+pub(crate) fn str_to_type(ty: &str) -> syn::Type {
     syn::Type::parse
         .parse_str(ty)
         .unwrap_or_else(|_| panic!("invalid type: {}", ty))
 }
 
-pub(super) fn default_buffer_name() -> syn::Ident {
+pub(crate) fn default_buffer_name() -> syn::Ident {
     quote::format_ident!("__buf")
 }
 
-pub(super) fn box_type() -> syn::Type {
+pub(crate) fn box_type() -> syn::Type {
     str_to_type("Box<dyn std::error::Error>")
 }
