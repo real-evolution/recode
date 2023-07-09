89aace02cc9db788c05744ffde81561c58c18fc2
Ayman Al-Qadhi <alqd@protonmail.com>
2023-07-09 21:51:18 +0300

    feat: add a utility method to get the size of current instance

--- recode/src/util/encoder.rs
+++ recode/src/util/encoder.rs
@@ -10,6 +10,11 @@ pub trait EncoderExt<B>: Encoder<B> + Sized {
     fn encode_to(&self, buf: &mut B) -> Result<(), Self::Error> {
         <Self as Encoder<B>>::encode(self, buf)
     }
+
+    /// Returns the number of bytes required to encode `self`.
+    fn size(&self, buf: &B) -> usize {
+        <Self as Encoder<B>>::size_of(self, buf)
+    }
 }
 
 impl<T, B> EncoderExt<B> for T where T: Encoder<B> + Sized {}
