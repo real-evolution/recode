c0e97d27b16170890616349f4f321fb59f609834
Ayman Al-Qadhi <alqd@protonmail.com>
2023-07-23 02:03:43 +0300

    doc(codec): inline docs for `codecs` module

--- recode/src/codec/mod.rs
+++ recode/src/codec/mod.rs
@@ -5,8 +5,13 @@ mod text;
 #[cfg(feature = "ux")]
 mod ux;
 
+#[doc(inline)]
 pub use buffer::*;
+#[doc(inline)]
+pub use bytes::*;
+#[doc(inline)]
 pub use number::*;
+#[doc(inline)]
 pub use text::*;
 
 #[cfg(feature = "ux")]
